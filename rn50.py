f35450d9165fc9450602f9e7ab6030261bde5a80
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-21 19:17:51 +0100

    Create rn50.py

---
+++ rn50.py
@@ -0,0 +1,18 @@
+from tensorflow.keras.applications import ResNet50
+from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
+from tensorflow.keras.preprocessing import image
+import numpy as np
+
+
+model = ResNet50(weights='imagenet')
+
+
+img_path = 'example.jpg'  # Път до изображението
+img = image.load_img(img_path, target_size=(224, 224)) 
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0) 
+x = preprocess_input(x)  
+
+# Предсказване
+predictions = model.predict(x)
+print('Predicted:', decode_predictions(predictions, top=3)[0])  
