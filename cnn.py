cfcb5f677f07cd63df380b6dbf7c8535011564ae
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-21 19:13:39 +0100

    Create cnn.py

---
+++ cnn.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+from tensorflow.keras import layers, models
+
+
+model = models.Sequential([
+    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(128, 128, 3)),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(128, (3, 3), activation='relu'),
+    layers.Flatten(),
+    layers.Dense(128, activation='relu'),
+    layers.Dense(1, activation='sigmoid') 
+])
+
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+model.summary()
